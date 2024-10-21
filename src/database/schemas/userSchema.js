375eac559265bb76a5319a656714faa6ae6da99c
Danilo Donado <danilodonadoa@gmail.com>
2024-10-21 08:58:56 -0400

    created database for users

---
+++ src/database/schemas/userSchema.js
@@ -0,0 +1,41 @@
+import { Schema } from "mongoose";
+import validator from "validator";
+
+//Defines a Schema to create users model  
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Please enter a name']
+    },
+    
+    email: {
+        type: String,
+        required: [true, 'Please enter an email'],
+        unique: true,
+        validate: [validator.isEmail, 'Please enter a valid email']
+    },
+
+    email_verified_at: {
+        type: Date, 
+        // required: [true, 'Timestamp is mandatory']
+    },
+
+    phone: String,
+
+    password: {
+        type: String,
+        required: [true, 'Please enter a password']
+    },
+
+    createdAt: {
+        type: Date,
+        // required: [true, 'Timestamp is mandatory']
+    },
+
+    updatedAt: {
+        type: Date,
+        // required: [true, 'Timestamp is mandatory']
+    }
+});
+
+export default userSchema; 
